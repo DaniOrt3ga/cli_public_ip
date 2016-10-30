d308d0fd9f43113d31b9a7dc247569db5e140124
DaniOrt3ga <daniortega82@gmail.com>
2016-10-30 00:05:55 -0300

    Add files via upload

---
+++ public_ip.c
@@ -0,0 +1,20 @@
+/* PROGRAMA PARA DETERMINAR DIRECCION IP PUBLICA DESDE LA LINEA DE COMANDO DE UNA MAQUINA LINUX
+	AUTOR=	Daniel Ortega
+	VERSION=	1.1
+*/
+
+#include <stdio.h>
+
+void main()
+{
+  FILE *fp;
+  char str[30];		//save the data from de command line
+   
+  fp = popen("dig TXT +short o-o.myaddr.l.google.com @ns1.google.com", "r");	//this is the command we pass to the command line
+
+  while ( fgets( str, sizeof str, fp))
+  {
+    printf("PUBLIC_IP = %s", str);
+  }
+  pclose(fp);
+}
